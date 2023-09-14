48df6ad36cd32d7835a9b9b6c68c2574920405ef
Sergey Goncharov <sersh@MacBook-Pro-Sergey.local>
2023-09-14 17:37:18 +0500

    Доработка первого блока

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+import * as globalFunctions from './modules/functions.js';
+globalFunctions.isWebp();
+
+import Vue from 'vue/dist/vue.js';
+import $ from 'jquery';
+
+// import Header from '../blocks/modules/header/header.js';
+
+window.app = new Vue({
+    el: '#app',
+    data: () => ({
+        isMounted: false,
+        sizes: {
+            tablet: 1024,
+            mobile: 768,
+            window: window.innerWidth
+        },
+    }),
+    beforeCreate() {        
+        window.addEventListener('resize', () => {
+            this.sizes.window = window.innerWidth;
+        });
+    },
+    beforeMount() {
+        this.isMounted = true;
+    },
+    computed: {
+        isMobile: function () {
+            return this.sizes.window < this.sizes.mobile;
+        },
+        isTablet: function () {
+            return this.sizes.window < this.sizes.tablet && this.sizes.window > this.sizes.mobile;
+        }
+    },
+});
